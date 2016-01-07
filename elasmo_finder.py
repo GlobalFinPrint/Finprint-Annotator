82ea0b4c3abd443e0244ee6208de1e936bd53c9c
Stuartg <jaxiano@gmail.com>
2016-01-07 09:21:54 -0800

    Update pluggable OD to integrate into frame rendering loop.

--- elasmo_finder.py
+++ elasmo_finder.py
@@ -13,46 +13,28 @@ class Hit(object):
 
 class ElasmoFinder(object):
     def __init__(self):
-        self._capture = None
         self._min_area = 4000
-        self.hits = []
+        self.fgbg = cv2.createBackgroundSubtractorMOG2()
 
-    def process_video(self, file_name):
-        self._capture = cv2.VideoCapture(file_name)
-        self._capture.set(cv2.CAP_PROP_FPS, 120)
-        fgbg = cv2.createBackgroundSubtractorMOG2()
+    def check_frame(self, frame):
+        hits = []
 
-        # loop over the frames of the video
-        while True:
-            (grabbed, frame) = self._capture.read()
+        #apply backgroundsubtractor
+        sub_frame = self.fgbg.apply(frame)
 
-            # if the frame could not be grabbed, then we have reached the end
-            # of the video
-            if not grabbed:
-                break
-
-            frame = imutils.resize(frame, width=900)
-
-            #apply backgroundsubtractor
-            sub_frame = fgbg.apply(frame)
-
-            # dilate the thresholded image to fill in holes, then find contours
-            # on thresholded image
-            thresh = cv2.dilate(sub_frame, None, iterations=2)
-            (img, cnts, hierarchy) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            #
-            # # loop over the contours
-            for c in cnts:
-                # if the contour is too small, ignore it
-                ca = cv2.contourArea(c)
-                if ca < self._min_area:
-                    continue
-
+        # dilate the thresholded image to fill in holes, then find contours
+        # on thresholded image
+        thresh = cv2.dilate(sub_frame, None, iterations=2)
+        (img, cnts, hierarchy) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        #
+        # # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            ca = cv2.contourArea(c)
+            if ca >= self._min_area:
                 h = Hit()
                 h.area = ca
                 h.bounding_rectangle = cv2.boundingRect(c)
-                h.position = self._capture.get(cv2.CAP_PROP_POS_MSEC)
-                self.hits.append(h)
+                hits.append(h)
 
-        # cleanup the camera and close any open windows
-        self._capture.release()
+        return hits
