b526685f9fb8bfae0b4a02d5ba0f9af489676084
Robert Reis <RobertR@vulcan.com>
2016-06-06 15:07:05 -0700

    fill in some todos for the client side of the api

--- global_finprint/global_finprint_server.py
+++ global_finprint/global_finprint_server.py
@@ -106,17 +106,38 @@ class GlobalFinPrintServer(Singleton):
         else:
             raise QueryException('Failed to delete observation: status {0}'.format(r.status_code))
 
-    def add_event(self, set_id, obs_id):
-        pass
+    def add_event(self, set_id, obs_id, **kwargs):
+        params = {'token': self.user_token}
+        params.update(kwargs)  # TODO filter out non-event stuff?
+        r = requests.post(self.address + '/api/set/{0}/obs/{1}/event'.format(set_id, obs_id), params)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise QueryException('Failed to add event: status {0}'.format(r.status_code))
 
-    def edit_event(self, set_id, obs_id):
-        pass
+    def edit_event(self, set_id, obs_id, evt_id, **kwargs):
+        params = {'token': self.user_token}
+        params.update(kwargs)  # TODO filter out non-event stuff?
+        r = requests.post(self.address + '/api/set/{0}/obs/{1}/event/{2}'.format(set_id, obs_id, evt_id), params)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise QueryException('Failed to edit event: status {0}'.format(r.status_code))
 
-    def delete_event(self, set_id, obs_id):
-        pass
+    def delete_event(self, set_id, obs_id, evt_id):
+        params = {'token': self.user_token, 'evt_id': evt_id}
+        r = requests.delete(self.address + '/api/set/{0}/obs/{1}/event'.format(set_id, obs_id), params=params)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise QueryException('Failed to delete event: status {0}'.format(r.status_code))
 
     def attributes(self, set_id):
-        pass  # TODO make sure we get tree structure
+        r = requests.get(self.address + '/api/set/{0}/attributes'.format(set_id))
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise QueryException('Failed to get attributes: status {0}'.format(r.status_code))
 
     def animals(self, set_id):
         r = requests.get(self.address + '/api/set/{0}/animals'.format(set_id), params={'token': self.user_token})
