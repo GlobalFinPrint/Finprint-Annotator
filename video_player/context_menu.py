6c09e6f135761ea19c2f4f1e3e67f2673c90d602
Robert Reis <RobertR@vulcan.com>
2016-07-15 11:34:50 -0700

    [GLOB-128] dont show animal dropdown on add evt to obs

--- video_player/context_menu.py
+++ video_player/context_menu.py
@@ -137,8 +137,7 @@ class EventDialog(QDialog):
             layout.addWidget(type_label)
 
         # obs animal (if applicable)
-        # TODO only editable on new obs/obs edit
-        if 'animal' in kwargs:
+        if kwargs['action'] in [DialogActions.new_obs, DialogActions.edit_obs]:
             animal_label = QLabel('Organism:')
             self.animal_dropdown = QComboBox()
             animal_label.setBuddy(self.animal_dropdown)
