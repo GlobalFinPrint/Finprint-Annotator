9bf4c854e9ab1032ada61d1f048734b1bca2c9da
Robert Reis <RobertR@vulcan.com>
2016-10-31 12:00:43 -0700

    mark capture as rbg instead of each frome (test)

--- video_player/cv_video_widget.py
+++ video_player/cv_video_widget.py
@@ -76,6 +76,7 @@ class FrameManager(object):
 
         self._capture.set(cv2.CAP_PROP_FRAME_WIDTH, VIDEO_WIDTH)
         self._capture.set(cv2.CAP_PROP_FRAME_HEIGHT, VIDEO_HEIGHT)
+        self._capture.set(cv2.CAP_PROP_CONVERT_RGB, True)
 
         self.FPS = self._capture.get(cv2.CAP_PROP_FPS)
         self.playback_FPS = self.FPS
@@ -356,7 +357,7 @@ class CvVideoWidget(QWidget):
             height, width, channels = frame.shape
             image = QImage(frame, width, height, QImage.Format_RGB888)
             # image = image.scaled(self._target_width(), self._target_height(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
-            image = image.rgbSwapped()
+            # image = image.rgbSwapped()
 
         except Exception as ex:
             getLogger('finprint').exception('Exception building image: {}'.format(str(ex)))
