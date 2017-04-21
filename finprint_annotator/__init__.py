5ef3c23b2f837e399c1e0ce5e87634e40a8c411f
Amit1603 <amit.kumar3@talentica.com>
2017-04-21 19:14:12 +0530

    GLOB:544: missing

--- finprint_annotator/__init__.py
+++ finprint_annotator/__init__.py
@@ -1,4 +1,4 @@
 from .assignment_widget import AssignmentWidget
 from .login_widget import LoginWidget
 from .main_window import MainWindow
-from .assignment_util import AssignmentFilter
+from .assignment_filter import AssignmentFilterDTO
